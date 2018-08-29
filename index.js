27bfa275c670273047aa2b57b95511143d54622e
Eric <moondawg2413@gmail.com>
2018-08-29 00:03:53 -0500

    Work on redoing server code

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// Imports
 const express = require("express")
-const app = express()
 const httpRequest = require("request")
+const rp = require("request-promise")
 const path = require("path")
-const csParse = require("pg-connection-string").parse // For parsing conneciton URL
+const csParse = require("pg-connection-string").parse
+const { Pool } = require("pg")
+const uuidv4 = require("uuid/v4")
+const app = express()
+
+// Config
 let bungieConfig
 
 try {
@@ -15,7 +21,7 @@ const clientId = process.env.CLIENT_ID || bungieConfig.client_id
 const clientSecret = process.env.CLIENT_SECRET || bungieConfig.client_secret
 const resources = process.env.RESOURCES || bungieConfig.resources
 
-// Set up DB connection
+// DB Connection
 const connectionString = process.env.DATABASE_URL || bungieConfig.database_url
 const dbConfig = csParse(connectionString)
 process.env.PGHOST = dbConfig.host
@@ -24,6 +30,36 @@ process.env.PGDATABASE = dbConfig.database
 process.env.PGPASSWORD = dbConfig.password
 process.env.PGPORT = dbConfig.port
 process.env.PGSSLMODE = "require"
+
+// Request Options
+const getAuthorizationRequestOptions = code => ({
+  url: "https://www.bungie.net/platform/app/oauth/token/",
+  method: "POST",
+  headers: {
+    "X-API-Key": apiKey,
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+  body: `grant_type=authorization_code&code=${code}&client_id=${clientId}&client_secret=${clientSecret}`,
+})
+
+const getRefreshRequestOptions = refreshToken => ({
+  url: "https://www.bungie.net/platform/app/oauth/token/",
+  method: "POST",
+  headers: {
+    "X-API-Key": apiKey,
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+  body: `grant_type=refresh_token&refresh_token=${refreshToken}&client_id=${clientId}&client_secret=${clientSecret}`,
+})
+
+const getUserRequestOptions = accessToken => ({
+  url: "http://www.bungie.net/Platform/User/GetCurrentBungieAccount/",
+  method: "GET",
+  headers: {
+    "X-API-Key": apiKey,
+    Authorization: `Bearer ${accessToken}`,
+  },
+})
 
 app.set("port", process.env.PORT || 5000)
 
@@ -47,37 +83,27 @@ app.get(["/", "/search", "/new", "/redirect"], function(request, response) {
 
 // Login
 app.get("/api/login", function(request, response) {
-  const options = {
-    url: "https://www.bungie.net/platform/app/oauth/token/",
-    method: "POST",
-    headers: {
-      "X-API-Key": apiKey,
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body:
-      "grant_type=authorization_code" +
-      "&code=" +
-      request.query.code +
-      "&client_id=" +
-      clientId +
-      "&client_secret=" +
-      clientSecret,
-  }
+  const options = getAuthorizationRequestOptions(request.query.code)
+  const cookieToken = uuidv4()
 
-  // Get tokens
-  httpRequest(options, function(err, httpResponse, body) {
-    if (err) {
-      response.statusCode = httpResponse.statusCode
-      response.send("An error has occurred!")
-      return
-    }
+  rp(options)
+    .then(response => {
+      const json = JSON.parse(response)
+      return {
+        cookieToken,
+        accessToken: json.access_token,
+        refreshToken: json.refresh_token,
+      }
+    })
+    .then(checkNinja)
+    .then(console.log)
+    .catch(err => {
+      response.statusCode = err.statusCode || 401
+      response.send("Get out of here, you're not welcome.")
+    })
 
-    const json = JSON.parse(httpResponse.body)
-
-    const uuidv4 = require("uuid/v4")
-    const cookieToken = uuidv4()
-
-    // Get Bnet profile info
+  /*
+     // Get Bnet profile info
     checkNinja(
       response,
       json.access_token,
@@ -106,6 +132,7 @@ app.get("/api/login", function(request, response) {
       },
     )
   })
+  */
 })
 
 // Get Resource Links
@@ -228,228 +255,100 @@ app.post("/api/new", function(request, response) {
   })
 })
 
-function getAccessTokens(response, cookieToken, returnCallback) {
-  executeQuery(
-    response,
-    "SELECT * FROM token WHERE cookie_token=$1;",
-    [cookieToken],
-    function(data) {
-      if (!data || data.length === 0) {
-        response.statusCode = 401
-        response.send("Unauthorized!")
-        return
-      }
+function checkNinja({ cookieToken, accessToken, refreshToken }) {
+  let model
 
-      if (data.length > 1) {
-        response.statusCode = 500
-        response.send(
-          "Multiple users found matching this cookie. Yell at MoonDawg to fix it!",
-        )
-        return
-      }
-
-      returnCallback(data[0])
-    },
-  )
-}
-
-function updateTokens(
-  response,
-  membershipId,
-  cookieToken,
-  accessToken,
-  refreshToken,
-  callback,
-) {
-  executeQuery(
-    response,
-    "SELECT * FROM token WHERE ninja_id=$1",
-    [membershipId],
-    function(data) {
-      // Update Tokens
-      if (data.length > 0) {
-        const query =
-          "UPDATE token SET access_token=$1, refresh_token=$2, cookie_token=$3 WHERE ninja_id=$4"
-        const params = [accessToken, refreshToken, cookieToken, membershipId]
-
-        executeQuery(response, query, params, function() {
-          callback()
-          return
-        })
-      }
-      // Insert Tokens
-      else {
-        const query =
-          "INSERT INTO token(ninja_id, access_token, refresh_token, cookie_token) VALUES($1, $2, $3, $4, $5);"
-        const params = [membershipId, accessToken, refreshToken, cookieToken]
-
-        executeQuery(response, query, params, function() {
-          callback()
-          return
-        })
-      }
-    },
-  )
-}
-
-function checkNinja(
-  response,
-  accessToken,
-  refreshToken,
-  cookieToken,
-  returnCallback,
-  nested,
-) {
-  // List of UserIds for current Ninjas
-  var arr = [
-    282, // stosh
-    1774, // Duardo
-    2526, // dmg04
-    17925, // FoMan123
-    22704, // Cozmo
-    5872880, // BNGHelp0
-    5872883, // BNGHelp1
-    8305888, // BNGHelp2
-    8306078, // BNGHelp3
-    8306295, // BNGHelp4
-    10813992, // BNGHelp5
-    1127, // dmbfan09
-    2311, // Shacker
-    2336, // Lord Revan (Sellout)
-    3352, // irishfreak
-    4878, // Spawn
-    6004, // Atsumi
-    7024, // Butane
-    29347, // DMHCook
-    31152, // onyx spartan
-    51262, // bobcast
-    60418, // ZER0COOL
-    68974, // dazarobbo
-    76974, // Old Papa Rich
-    83901, // Froggie
-    95799, // Plain Ben
-    108762, // MoonDawg
-    218625, // Index
-    328524, // Phoenix1710
-    398983, // Takedown
-    446470, // machinimagames
-    924570, // Needless
-    964968, // Makeshyft
-    1093770, // Seraphim Crypto
-    1292876, // Fuzzle
-    2985263, // BaghdadBean
-    3537380, // Commander Scurvy
-    4864368, // Ecto1italia
-    5666728, // Dimitrios4
-    5696684, // Psyperactive81
-    5828411, // Siffow
-    17956, // Cortana V
-    8878733, // truexadir
-  ]
-
-  const userOptions = {
-    url: "http://www.bungie.net/Platform/User/GetCurrentBungieAccount/",
-    method: "GET",
-    headers: {
-      "X-API-Key": apiKey,
-      Authorization: "Bearer " + accessToken,
-    },
-  }
-
-  httpRequest(userOptions, function callback(err, httpResponse, body) {
-    console.log("HTTP Status Code: " + httpResponse.statusCode)
-
-    // Valid user
-    if (httpResponse.statusCode === 200) {
-      const json = JSON.parse(httpResponse.body).Response.bungieNetUser
-
-      // Make sure authenticated user is a Ninja
-      if (arr.indexOf(parseInt(json.membershipId, 10)) > -1) {
-        const model = {
-          MembershipId: json.membershipId,
-          Name: json.displayName,
-          Avatar_URL: "http://www.bungie.net" + json.profilePicturePath,
-          Access_Token: accessToken,
-          Refresh_Token: refreshToken,
-        }
-
-        returnCallback(model)
-        return
-      }
-    }
-    // Invalid/Expired Access Token
-    else if (httpResponse.statusCode === 401 && !nested) {
-      const refreshOptions = {
-        url: "https://www.bungie.net/platform/app/oauth/token/",
-        method: "POST",
-        headers: {
-          "X-API-Key": apiKey,
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body:
-          "grant_type=refresh_token" +
-          "&refresh_token=" +
-          refreshToken +
-          "&client_id=" +
-          clientId +
-          "&client_secret=" +
-          clientSecret,
-      }
-
-      // Get new Access Token
-      httpRequest(refreshOptions, function(err, httpResponse, body) {
-        if (err) {
-          response.statusCode = httpResponse.statusCode
-          response.send("An error has occurred!")
-          return
-        }
-
-        const json = JSON.parse(httpResponse.body)
-
-        // Get Bnet profile info & set nested flag (prevents infinite loop)
-        checkNinja(
-          response,
-          json.access_token,
-          json.refresh_token,
-          cookieToken,
-          function(user) {
-            // Update tokens in database
-            updateTokens(
-              response,
-              user.MembershipId,
-              cookieToken,
-              json.access_token,
-              json.refresh_token,
-              function() {
-                returnCallback(user)
-                return
-              },
-            )
-          },
-          true,
-        ) // Flag prevents infinite loop
+  return (
+    getUser(accessToken)
+      // Valid Access Token
+      .then(response => {
+        const user = JSON.parse(response).Response.bungieNetUser
+        model = generateModel(user, accessToken, refreshToken)
       })
+      // Invalid Access Token - Use Refresh Token
+      .catch(() => {
+        const options = getRefreshRequestOptions(refreshToken)
 
-      return
+        return rp(options).then(response => {
+          const tokens = JSON.parse(response)
+
+          return getUser(tokens.access_token).then(response => {
+            const user = JSON.parse(response).Response.bungieNetUser
+            console.log(user)
+
+            // Update tokens in DB
+            updateTokens(
+              user.membershipId,
+              cookieToken,
+              tokens.access_token,
+              tokens.refresh_token,
+            )
+
+            model = generateModel(
+              user,
+              tokens.access_token,
+              tokens.refresh_token,
+            )
+          })
+        })
+      })
+      // Check Authorized Ninja
+      .then(() =>
+        executeQuery("SELECT display_name FROM ninja WHERE ninja_id = $1", [
+          model.membershipId,
+        ]),
+      )
+      .then(result => {
+        if (result.length == 0) throw new Error("Unauthorized")
+      })
+      .then(() => model)
+  )
+}
+
+const getUser = accessToken => {
+  const options = getUserRequestOptions(accessToken)
+  return rp(options)
+}
+
+const generateModel = (user, accessToken, refreshToken) => ({
+  membershipId: user.membershipId,
+  name: user.displayName,
+  avatarURL: `http://www.bungie.net${user.profilePicturePath}`,
+  accessToken,
+  refreshToken,
+})
+
+const updateTokens = (membershipId, cookieToken, accessToken, refreshToken) => {
+  executeQuery("SELECT ninja_id FROM token WHERE ninja_id=$1", [
+    membershipId,
+  ]).then(result => {
+    let query, params
+
+    // Update Tokens
+    if (result.length > 0) {
+      query =
+        "UPDATE token SET access_token=$1, refresh_token=$2, cookie_token=$3 WHERE ninja_id=$4"
+      params = [accessToken, refreshToken, cookieToken, membershipId]
+    }
+    // Insert Tokens
+    else {
+      query =
+        "INSERT INTO token(ninja_id, access_token, refresh_token, cookie_token) VALUES($1, $2, $3, $4);"
+      params = [membershipId, accessToken, refreshToken, cookieToken]
     }
 
-    response.statusCode = 401
-    response.send("Hey... you're not a Ninja! Shoo!")
+    executeQuery(query, params)
   })
 }
 
-function executeQuery(response, query, values, callback) {
-  const { Pool } = require("pg")
-  var pool = new Pool()
+const executeQuery = (query, params) => {
+  const pool = new Pool()
 
-  pool.query(query, values, (err, res) => {
-    if (err) {
+  return pool
+    .query(query, params)
+    .then(res => res.rows)
+    .catch(err => {
       console.log(err)
-      response.statusCode = 500
-      response.send("SQL error")
-    }
-
-    pool.end()
-    callback(res.rows)
-  })
+      throw err
+    })
 }
