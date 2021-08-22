45dfec1ba578fdbb7435bc2319bfcac8bbd140b0
Eric Moon <eric.moon@carebridgehealth.com>
2021-08-21 19:41:28 -0500

    Fix styling

--- src/components/AuthorizedUser.js
+++ src/components/AuthorizedUser.js
@@ -5,17 +5,17 @@ import { withStyles } from "@material-ui/core/styles"
 const styles = ({ spacing }) => ({
   container: {
     display: "block",
-    textAlign: "right"
+    textAlign: "right",
   },
   logOut: {
     cursor: "pointer",
-    textDecoration: "underline"
+    textDecoration: "underline",
   },
   avatar: {
     width: 45,
     height: 45,
-    marginLeft: spacing.unit * 2
-  }
+    marginLeft: spacing.unit * 2,
+  },
 })
 
 class AuthorizedUser extends Component {
@@ -26,19 +26,20 @@ class AuthorizedUser extends Component {
       user && (
         <React.Fragment>
           <div className={classes.container}>
-            <Typography variant="subheading" color="inherit">
+            <Typography component='p' variant='subheading' color='inherit'>
               Welcome {user.name}!
             </Typography>
             <Typography
               className={classes.logOut}
-              variant="subheading"
-              color="inherit"
+              component='p'
+              variant='subheading'
+              color='inherit'
               onClick={onLogOut}
             >
               Log Out
             </Typography>
           </div>
-          <img className={classes.avatar} src={user.avatarURL} alt="avatar" />
+          <img className={classes.avatar} src={user.avatarURL} alt='avatar' />
         </React.Fragment>
       )
     )
