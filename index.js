1be1e3556084e5af1eb73cd00992adf6a4201331
Eric Moon <eric.moon@carebridgehealth.com>
2021-08-24 15:49:28 -0500

    Completely rework server logic

--- index.js
+++ index.js
@@ -21,8 +21,9 @@ const clientSecret = process.env.CLIENT_SECRET || bungieConfig.client_secret
 const resources = process.env.RESOURCES || bungieConfig.resources
 const connectionString = process.env.DATABASE_URL || bungieConfig.database_url
 
-const fetch = (url, options) =>
-  nodeFetch(url, options).then((response) => response.json())
+function fetch(url, options) {
+  return nodeFetch(url, options).then((response) => response.json())
+}
 
 // Request Options
 const getAuthorizationRequestOptions = (code) => ({
@@ -51,24 +52,37 @@ const getRequestOptions = (accessToken) => ({
   },
 })
 
+// Model
+const generateModel = (user, cookieToken, accessToken, refreshToken) => ({
+  membershipId: user.membershipId,
+  name: user.displayName,
+  avatarURL: `http://www.bungie.net${user.profilePicturePath}`,
+  resources,
+  cookieToken,
+  accessToken,
+  refreshToken,
+})
+
 const OK = 200
 const UNAUTHORIZED = 401
 const NOT_FOUND = 404
 const SERVER_ERROR = 500
 
-const getErrorMessage = (statusCode) => {
+function getErrorMessage(statusCode) {
   switch (statusCode) {
-    case 401:
+    case UNAUTHORIZED:
       return "Hey, you're not allowed to be here. Shoo!"
-    case 404:
+    case NOT_FOUND:
       return "Clan not found!"
-    case 500:
+    case SERVER_ERROR:
     default:
       return "An internal server error has occurred... yell at MoonDawg to fix it."
   }
 }
 
-const getAuthToken = (request) => request.headers.authorization.substring(6)
+function getAuthToken(request) {
+  return request.headers.authorization.substring(6)
+}
 
 app.set("port", process.env.PORT || 5000)
 
@@ -97,51 +111,55 @@ app.get("/api/client_id", (_request, response) => {
 })
 
 // Login
-app.get("/api/login", (request, response) => {
-  const options = getAuthorizationRequestOptions(request.query.code)
-  const cookieToken = uuidv4()
+app.get("/api/login", async (request, response) => {
+  try {
+    const cookieToken = uuidv4()
 
-  fetch("https://www.bungie.net/platform/app/oauth/token/", options)
-    .catch(({ statusCode }) => {
-      throw statusCode
-    })
-    .then((data) => {
-      return checkNinja(cookieToken, data.access_token, data.refresh_token)
-    })
-    .then(({ accessToken, refreshToken, ...model }) => {
-      return updateTokens(
-        model.membershipId,
-        model.cookieToken,
-        accessToken,
-        refreshToken
-      ).then(() => model)
-    })
-    .then((model) => {
-      return getRegion(model.membershipId).then((region) => ({
-        ...model,
-        region,
-      }))
-    })
-    .then((model) => {
-      response.statusCode = OK
-      response.send(model)
-    })
-    .catch((statusCode) => {
-      response.statusCode = statusCode
-      response.statusMessage = getErrorMessage(statusCode)
-      response.end()
-    })
+    const options = getAuthorizationRequestOptions(request.query.code)
+    const { access_token, refresh_token } = await fetch(
+      "https://www.bungie.net/platform/app/oauth/token/",
+      options
+    )
+
+    const resp = await getUser(access_token)
+    const user = resp.Response.bungieNetUser
+
+    const model = generateModel(user, cookieToken, access_token, refresh_token)
+
+    const { region } = await executeSingleQuery(
+      "SELECT * FROM ninja WHERE ninja_id=$1",
+      [model.membershipId],
+      UNAUTHORIZED
+    )
+
+    // Update tokens in database
+    await updateTokens(
+      model.membershipId,
+      model.cookieToken,
+      access_token,
+      refresh_token
+    )
+
+    response.statusCode = OK
+    response.send({ ...model, region })
+  } catch (err) {
+    const statusCode = err.statusCode || err
+
+    response.statusCode = statusCode
+    response.statusMessage = getErrorMessage(statusCode)
+    response.end()
+  }
 })
 
 // Search Clan Reports
-app.get("/api/search", (request, response) => {
-  const queryString = request.query
-  const token = getAuthToken(request)
+app.get("/api/search", async (request, response) => {
+  try {
+    const queryString = request.query
+    const token = getAuthToken(request)
 
-  getAccessTokens(token)
-    .then((data) => checkNinja(token, data.access_token, data.refresh_token))
-    .then((user) => {
-      let query = `
+    const { membershipId } = await getNinja(token)
+
+    let query = `
         SELECT
           r.id,
           r.report_date AS "reportDate",
@@ -156,78 +174,74 @@ app.get("/api/search", (request, response) => {
         JOIN ninja n ON n.ninja_id = r.ninja_id
         WHERE 1=1
         `
-      let paramNum = 1
-      let values = []
+    let paramNum = 1
+    let values = []
 
-      if (queryString.clan_id) {
-        query += ` AND r.clan_id=$${paramNum}`
-        paramNum++
-        values.push(queryString.clan_id)
-      }
+    if (queryString.clan_id) {
+      query += ` AND r.clan_id=$${paramNum}`
+      paramNum++
+      values.push(queryString.clan_id)
+    }
 
-      if (queryString.clan_name) {
-        query += ` AND r.clan_name ~* $${paramNum}`
-        paramNum++
-        values.push(queryString.clan_name)
-      }
+    if (queryString.clan_name) {
+      query += ` AND r.clan_name ~* $${paramNum}`
+      paramNum++
+      values.push(queryString.clan_name)
+    }
 
-      if (queryString.last_100_regional_reports) {
-        query += ` AND r.region=$${paramNum}`
-        paramNum++
-        values.push(queryString.region)
-      }
+    if (queryString.last_100_regional_reports) {
+      query += ` AND r.region=$${paramNum}`
+      paramNum++
+      values.push(queryString.region)
+    }
 
-      if (queryString.user_100_reports) {
-        query += ` AND r.ninja_id=$${paramNum}`
-        paramNum++
-        values.push(user.membershipId)
-      }
+    if (queryString.user_100_reports) {
+      query += ` AND r.ninja_id=$${paramNum}`
+      paramNum++
+      values.push(membershipId)
+    }
 
-      query += " ORDER BY r.report_date DESC"
+    query += " ORDER BY r.report_date DESC"
 
-      if (
-        queryString.last_100_reports ||
-        queryString.last_100_regional_reports ||
-        queryString.user_100_reports
-      ) {
-        query += " LIMIT 100"
-      }
+    if (
+      queryString.last_100_reports ||
+      queryString.last_100_regional_reports ||
+      queryString.user_100_reports
+    ) {
+      query += " LIMIT 100"
+    }
 
-      return executeQuery(query, values)
-    })
-    .then((data) => {
-      response.statusCode = OK
-      response.send(data)
-    })
-    .catch((statusCode) => {
-      response.statusCode = statusCode
-      response.statusMessage = getErrorMessage(statusCode)
-      response.end()
-    })
+    const data = await executeQuery(query, values)
+
+    response.statusCode = OK
+    response.send(data)
+  } catch (err) {
+    const statusCode = err.statusCode || err
+
+    response.statusCode = statusCode
+    response.statusMessage = getErrorMessage(statusCode)
+    response.end()
+  }
 })
 
 // Get Clan info from Bnet
 app.get("/api/clan", async (request, response) => {
-  const clanId = request.query.clan_id
-  const token = getAuthToken(request)
-
   try {
-    const tokens = await getAccessTokens(token)
+    const clanId = request.query.clan_id
 
-    const userInfo = await checkNinja(
-      token,
-      tokens.access_token,
-      tokens.refresh_token
-    )
+    const token = getAuthToken(request)
+    const { accessToken } = await getNinja(token)
 
-    const options = getRequestOptions(userInfo.accessToken)
+    const options = getRequestOptions(accessToken)
 
     const clanInfo = await fetch(
       `https://www.bungie.net/Platform/GroupV2/${clanId}`,
       options
     )
 
-    if (clanInfo.ErrorStatus === "GroupNotFound") throw NOT_FOUND
+    if (clanInfo.ErrorStatus === "GroupNotFound") {
+      throw NOT_FOUND
+    }
 
     const clan = clanInfo.Response.detail
 
@@ -240,7 +254,9 @@ app.get("/api/clan", async (request, response) => {
 
     response.statusCode = OK
     response.send(data)
-  } catch (statusCode) {
+  } catch (err) {
+    const statusCode = err.statusCode || err
+
     response.statusCode = statusCode
     response.statusMessage = getErrorMessage(statusCode)
     response.end()
@@ -248,65 +264,55 @@ app.get("/api/clan", async (request, response) => {
 })
 
 // New Clan Report
-app.post("/api/new", (request, response) => {
-  const token = getAuthToken(request)
+app.post("/api/new", async (request, response) => {
+  try {
+    const token = getAuthToken(request)
+    const { membershipId } = await getNinja(token)
 
-  getAccessTokens(token)
-    .then((data) => checkNinja(token, data.access_token, data.refresh_token))
-    .then(
-      (user) =>
-        new Promise((resolve) => {
-          let body = ""
+    const data = await parseBody(request)
 
-          request
-            .on("data", (chunk) => {
-              body += chunk
-            })
-            .on("end", () => {
-              const data = JSON.parse(body)
+    const query = `
+      INSERT INTO report(clan_id, clan_name, clan_motto, clan_mission_statement, notes, ninja_id, judgment, region, report_date)
+      VALUES($1, $2, $3, $4, $5, $6, $7, $8, NOW());
+    `
 
-              const query = `
-                INSERT INTO report(clan_id, clan_name, clan_motto, clan_mission_statement, notes, ninja_id, judgment, region, report_date)
-                VALUES($1, $2, $3, $4, $5, $6, $7, $8, NOW());
-              `
+    const params = [
+      data.id,
+      data.name,
+      data.motto,
+      data.missionStatement,
+      data.notes,
+      membershipId,
+      data.judgment,
+      data.region,
+    ]
 
-              const params = [
-                data.id,
-                data.name,
-                data.motto,
-                data.missionStatement,
-                data.notes,
-                user.membershipId,
-                data.judgment,
-                data.region,
-              ]
+    await executeQuery(query, params)
 
-              resolve(executeQuery(query, params))
-            })
-        })
-    )
-    .then(() => {
-      response.statusCode = OK
-      response.send()
-    })
-    .catch((statusCode) => {
-      response.statusCode = statusCode
-      response.statusMessage = getErrorMessage(statusCode)
-      response.end()
-    })
+    response.statusCode = OK
+    response.send()
+  } catch (err) {
+    const statusCode = err.statusCode || err
+
+    response.statusCode = statusCode
+    response.statusMessage = getErrorMessage(statusCode)
+    response.end()
+  }
 })
 
-const checkNinja = async (cookieToken, accessToken, refreshToken) => {
+async function getNinja(cookieToken) {
   let model
 
+  const { access_token, refresh_token } = await getAccessTokens(cookieToken)
+
   try {
-    const resp = await getUser(accessToken)
+    const resp = await getUser(access_token)
     const user = resp.Response.bungieNetUser
 
-    model = generateModel(user, cookieToken, accessToken, refreshToken)
+    model = generateModel(user, cookieToken, access_token, refresh_token)
   } catch {
     // Invalid Access Token - Use Refresh Token
-    const options = getRefreshRequestOptions(refreshToken)
+    const options = getRefreshRequestOptions(refresh_token)
     const tokens = await fetch(
       "https://www.bungie.net/platform/app/oauth/token/",
       options
@@ -343,7 +349,7 @@ const checkNinja = async (cookieToken, accessToken, refreshToken) => {
   return model
 }
 
-const getUser = (accessToken) => {
+function getUser(accessToken) {
   const options = getRequestOptions(accessToken)
 
   return fetch(
@@ -352,57 +358,64 @@ const getUser = (accessToken) => {
   )
 }
 
-const generateModel = (user, cookieToken, accessToken, refreshToken) => ({
-  membershipId: user.membershipId,
-  name: user.displayName,
-  avatarURL: `http://www.bungie.net${user.profilePicturePath}`,
-  resources,
+async function getAccessTokens(cookieToken) {
+  const data = await executeQuery(
+    "SELECT * FROM token WHERE cookie_token=$1;",
+    [cookieToken]
+  )
+
+  if (data.length === 0) {
+    throw UNAUTHORIZED
+  } else if (data.length > 1) {
+    throw SERVER_ERROR
+  }
+
+  return data[0]
+}
+
+async function updateTokens(
+  membershipId,
   cookieToken,
   accessToken,
-  refreshToken,
-})
+  refreshToken
+) {
+  const data = await executeQuery(
+    "SELECT ninja_id FROM token WHERE ninja_id=$1",
+    [membershipId]
+  )
 
-const getAccessTokens = (cookieToken) =>
-  executeQuery("SELECT * FROM token WHERE cookie_token=$1;", [
-    cookieToken,
-  ]).then((data) => {
-    if (!data || data.length === 0) {
-      throw UNAUTHORIZED
-    } else if (data.length > 1) {
-      throw SERVER_ERROR
-    }
-
-    return data[0]
-  })
-
-const updateTokens = (membershipId, cookieToken, accessToken, refreshToken) =>
-  executeQuery("SELECT ninja_id FROM token WHERE ninja_id=$1", [
-    membershipId,
-  ]).then((result) => {
-    let query, params
-
+  if (data.length > 0) {
     // Update Tokens
-    if (result.length > 0) {
-      query =
-        "UPDATE token SET access_token=$1, refresh_token=$2, cookie_token=$3 WHERE ninja_id=$4"
-      params = [accessToken, refreshToken, cookieToken, membershipId]
-    }
+    await executeQuery(
+      "UPDATE token SET access_token=$1, refresh_token=$2, cookie_token=$3 WHERE ninja_id=$4",
+      [accessToken, refreshToken, cookieToken, membershipId]
+    )
+  } else {
     // Insert Tokens
-    else {
-      query =
-        "INSERT INTO token(ninja_id, access_token, refresh_token, cookie_token) VALUES($1, $2, $3, $4);"
-      params = [membershipId, accessToken, refreshToken, cookieToken]
-    }
+    await executeQuery(
+      "INSERT INTO token(ninja_id, access_token, refresh_token, cookie_token) VALUES($1, $2, $3, $4);",
+      [membershipId, accessToken, refreshToken, cookieToken]
+    )
+  }
+}
 
-    executeQuery(query, params)
+function parseBody(request) {
+  return new Promise((resolve) => {
+    let body = ""
+
+    request
+      .on("data", (chunk) => {
+        body += chunk
+      })
+      .on("end", () => {
+        const data = JSON.parse(body)
+
+        resolve(data)
+      })
   })
+}
 
-const getRegion = (membershipId) =>
-  executeQuery("SELECT region FROM ninja WHERE ninja_id=$1", [
-    membershipId,
-  ]).then((result) => result[0].region)
-
-const executeQuery = (query, params) => {
+async function executeQuery(query, params) {
   const pool = new Pool({
     connectionString,
     ssl: {
@@ -410,13 +423,26 @@ const executeQuery = (query, params) => {
     },
   })
 
-  return pool
-    .query(query, params)
-    .then((res) => {
-      return res.rows
-    })
-    .catch((err) => {
-      console.error(err)
-      throw SERVER_ERROR
-    })
+  try {
+    const { rows } = await pool.query(query, params)
+
+    return rows
+  } catch (err) {
+    console.error(err)
+
+    throw SERVER_ERROR
+  }
+}
+
+async function executeSingleQuery(query, params, exceptionType = SERVER_ERROR) {
+  const data = await executeQuery(query, params)
+
+  if (data.length === 0) {
+    throw UNAUTHORIZED
+  }
+  if (data.length !== 1) {
+    throw SERVER_ERROR
+  }
+
+  return data[0]
 }
