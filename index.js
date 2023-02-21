d100e64ca27a5a602bf55f9de37906a1d387dd29
Eric Moon <moondawg2413@gmail.com>
2023-02-21 16:26:27 -0600

    Need to actually include node packages

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function getAuthToken(request) {
   return request.headers.authorization.substring(6)
 }
 
-app.set("port", process.env.PORT || 5000)
+app.set("port", process.env.PORT || 5001)
 
 app.listen(app.get("port"), () => {
   console.log("Node app is running on port", app.get("port"))
