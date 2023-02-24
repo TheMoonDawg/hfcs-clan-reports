bfbf36d96a0b837a2c1cde0ff1b6ffbe6326a539
Eric Moon <moondawg2413@gmail.com>
2023-02-23 19:38:38 -0600

    New build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          mui: ['@mui/material'],
+        },
+      },
+    },
+  },
 })
